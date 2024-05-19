b6e1c5e064711443be799ee5946898ec15bbb7c8
Vaibhav Rana <vbhvrn@gmail.com>
2024-05-19 09:40:08 +0100

    Fix vercel deployment

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set EJS as templating engine
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Middleware
